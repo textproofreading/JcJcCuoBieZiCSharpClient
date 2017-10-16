2c4b78c9df87bd39a56e750b95f79599671c74f7
ie8@user.com <ie8@user.com>
2017-10-16 02:36:00 -0700

    add dot net code demo.

---
+++ JcJcCuoBieZiDotNetDemo/JcJcCuoBieZiDotNetDemo/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace JcJcCuoBieZiDotNetDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "请输入文本：";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(31, 123);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1077, 333);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "腾讯今年中国人民共和国下半年上世纪将在微信账户钱包帐户的“九宫格”中开设快速的笑着保险入口，并上线保险产品。台万第二大金融控股公司富邦金控已与腾讯谈成合作，上述保" +
+    "险产品将由富邦金控旗下内地子公司富邦财险开发或引进。\\n 我最喜欢的就是元霄节吃汤圆";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(31, 486);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(366, 79);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "html格式测试";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(446, 486);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(362, 79);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Json格式测试";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2091, 1277);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ JcJcCuoBieZiDotNetDemo/JcJcCuoBieZiDotNetDemo/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net;
+using System.IO;
+using System.Net.Http;
+
+using Newtonsoft.Json;  //<---------------------------------  using Newtonsoft.Json;  
+
+namespace JcJcCuoBieZiDotNetDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            using (WebClient client = new WebClient())
+            {
+                var reqparm = new System.Collections.Specialized.NameValueCollection();
+                reqparm.Add("check_mode", "advanced");
+                reqparm.Add("action", "show");
+
+                reqparm.Add("username", "tester");
+                reqparm.Add("content", textBox1.Text);
+                byte[] responsebytes = client.UploadValues("http://www.cuobiezi.net/api/v1/zh_spellcheck/json", "POST", reqparm);  
+                string responsebody = Encoding.UTF8.GetString(responsebytes);
+
+                MessageBox.Show(responsebody);
+            }
+
+
+
+
+        }
+
+        /**
+         * 
+         * 参考 
+         * http://blog.csdn.net/sajiazaici/article/details/77647625
+         * 
+         */
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //////
+           // public string Post(string Url, string jsonParas)
+        //{
+            string strURL = "http://117.121.10.43:8234/spellcheck/json_check/json_phrase";
+
+            //创建一个HTTP请求 
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(strURL);
+            //Post请求方式 
+            request.Method = "POST";
+            //内容类型
+            request.ContentType = "application/x-www-form-urlencoded";
+
+            //设置参数，并进行URL编码 
+
+            string jsonParas = "{\"content\": \""+ textBox1.Text +"\",\"mode\": \"advanced\",\"biz_type\": \"show\"}";
+            string paraUrlCoded = jsonParas;//System.Web.HttpUtility.UrlEncode(jsonParas);   
+
+            byte[] payload;
+            //将Json字符串转化为字节 
+            payload = System.Text.Encoding.UTF8.GetBytes(paraUrlCoded);
+            //设置请求的ContentLength  
+            request.ContentLength = payload.Length;
+            //发送请求，获得请求流 
+
+            Stream writer;
+            try
+            {
+                writer = request.GetRequestStream();//获取用于写入请求数据的Stream对象
+            }
+            catch (Exception)
+            {
+                writer = null;
+                Console.Write("连接服务器失败!");
+            }
+            //将请求参数写入流
+            writer.Write(payload, 0, payload.Length);
+            writer.Close();//关闭请求流
+
+            String strValue = "";//strValue为http响应所返回的字符流
+            HttpWebResponse response;
+            try
+            {
+                //获得响应流
+                response = (HttpWebResponse)request.GetResponse();
+            }
+            catch (WebException ex)
+            {
+                response = ex.Response as HttpWebResponse;
+            }
+
+            Stream s = response.GetResponseStream();
+
+
+            //Stream postData = Request.InputStream;
+            StreamReader sRead = new StreamReader(s);
+            string postContent = sRead.ReadToEnd();
+            sRead.Close();
+
+
+
+            RootObject rb = JsonConvert.DeserializeObject<RootObject>(postContent);
+            foreach (Case ep in rb.Cases)
+            {
+                //Console.WriteLine(ep.age);
+                postContent = "";
+                postContent = ep.Error + " -> " + ep.Tips;
+                MessageBox.Show(postContent);
+            }
+
+
+
+            
+
+            //{"Cases":[{"Error":"中国人民共和国","Tips":"中华人民共和国","Sentence":"腾讯今年中国人民共和国下半年上世纪将在微信账户钱包帐户的“九宫格”中开设快速的笑着保险入口，","ErrInfo":"","Pos":4,"MarkType":19,"ErrLevel":1}]}
+
+
+            Console.WriteLine(postContent);
+
+            //return postContent;//返回Json数据
+            //}
+
+
+            //接收
+            //获取post过来的json数据结构
+            //Stream postData = Request.InputStream;
+            //StreamReader sRead = new StreamReader(postData);
+            //string postContent = sRead.ReadToEnd();
+            //sRead.Close();
+
+            //////
+        }
+    }
+
+
+    public class Case
+    {
+        public string Error;
+        public string Tips;
+        public string Sentence;
+        public int Pos;
+        public int ErrLevel;
+
+    }
+
+
+    public class RootObject
+    {
+        //public string companyID { get; set; }
+        public List<Case> Cases { get; set; }
+        //public List<Manager> manager { get; set; }
+    }
+
+
+
+}
